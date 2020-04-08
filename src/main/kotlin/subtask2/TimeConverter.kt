307ba4e37cca699345169a9169ffc77f57ae66c7
LisPI <paschka.lis@gmail.com>
2020-04-09 00:01:33 +0300

    feat: add solution for DateFormatter, TimeConverter, Fibonacci

--- src/main/kotlin/subtask2/TimeConverter.kt
+++ src/main/kotlin/subtask2/TimeConverter.kt
@@ -2,8 +2,55 @@ package subtask2
 
 class TimeConverter {
 
-    // TODO: Complete the following function
     fun toTextFormat(hour: String, minute: String): String {
-        throw NotImplementedError("Not implemented")
+        val h = hour.toInt()
+        val m = minute.toInt()
+
+        if((h > 12) or (m > 60))
+            return ""
+
+        val words = arrayOf(
+            "",
+            "one",
+            "two",
+            "three",
+            "four",
+            "five",
+            "six",
+            "seven",
+            "eight",
+            "nine",
+            "ten",
+            "eleven",
+            "twelve",
+            "thirteen",
+            "fourteen",
+            "fifteen",
+            "sixteen",
+            "seventeen",
+            "eighteen",
+            "nineteen",
+            "twenty",
+            "twenty one",
+            "twenty two",
+            "twenty three",
+            "twenty four",
+            "twenty five",
+            "twenty six",
+            "twenty seven",
+            "twenty eight",
+            "twenty nine")
+
+        return when {
+            m == 0 -> "${words[h]} o' clock"
+            m == 30 -> "half past ${words[h]}"
+            m == 15 -> "quarter past ${words[h]}"
+            m == 45 -> "quarter to ${words[h + 1]}"
+            m == 1 -> "one minute past ${words[h]}"
+            m < 30 -> "${words[m]} minutes past ${words[h]}"
+            m > 30 -> "${words[60 - m]} minutes to ${words[h + 1]}"
+
+            else -> ""
+        }
     }
 }

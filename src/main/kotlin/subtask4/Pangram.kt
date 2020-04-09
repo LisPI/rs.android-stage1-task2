1d56a0d23e88112d87e017b23a8202d920072afe
LisPI <paschka.lis@gmail.com>
2020-04-10 00:52:07 +0300

    feat: add solution for Pangram - change it!

--- src/main/kotlin/subtask4/Pangram.kt
+++ src/main/kotlin/subtask4/Pangram.kt
@@ -1,9 +1,27 @@
 package subtask4
 
+import java.lang.StringBuilder
+
 class Pangram {
 
-    // TODO: Complete the following function
     fun getResult(inputString: String): String {
-        throw NotImplementedError("Not implemented")
+
+        var abc = mutableSetOf<Char>()
+        abc.addAll(inputString.toLowerCase().asIterable().filter { it in 'a'..'z' })
+        val isPangram = abc.size == 26
+
+        // TODO remove REGEX!!!!
+        val vowels = setOf('a','e','i','o','u','y')
+        val regex = "[aeiouy]".toRegex()
+        val regex2 = "[bcdfghj-np-tvwxz]".toRegex()
+
+        var words = inputString.replace('\n', ' ').split(' ').filter { it -> it.isNotEmpty() } as MutableList
+        for(index in words.indices)
+            words[index] = if(isPangram) words[index].count{ c -> vowels.contains(c.toLowerCase())}.toString() + regex.replace(words[index]){m -> m.value.toUpperCase() }
+                else words[index].count{ c -> !vowels.contains(c.toLowerCase()) and (c.toLowerCase() in 'a'..'z')}.toString() + regex2.replace(words[index]){m -> m.value.toUpperCase() }
+
+        words.sortWith(compareBy { it -> it[0] })
+
+        return words.joinToString(" ")
     }
 }
